f49c359bb68a72e8770b699703ff7face8781c17
maheshmb8 <140077235+maheshmb8@users.noreply.github.com>
2023-07-21 10:38:16 +0530

    Update Slit_Resume2.py

--- Slit_Resume2.py
+++ Slit_Resume2.py
@@ -198,7 +198,7 @@ Professional_Experience = {
 # Other Variables
 email_emoji = emoji.emojize(':envelope:')
 phone_emoji = emoji.emojize(':telephone_receiver:')
-Linkedin_URL_emoji = emoji.emojize(':link:')
+Linkedin_URL_emoji = emoji.emojize(':up_right_arrow:')
 Location_emoji = emoji.emojize(':house:')
 birthday_emoji = emoji.emojize(':birthday_cake:')
 
