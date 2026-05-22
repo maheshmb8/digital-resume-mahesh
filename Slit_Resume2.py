68ab0e9eb9d044d41ee35042de6e6308c1dd2be8
Mahesh Bathija <140077235+maheshmb8@users.noreply.github.com>
2026-05-22 10:55:47 +0530

    Update Slit_Resume2.py

--- Slit_Resume2.py
+++ Slit_Resume2.py
@@ -386,7 +386,7 @@ fig = px.treemap(
 
 # ---- Styling to match your resume theme ----
 fig.update_traces(
-    textinfo="label",
+    textinfo="Skills",
     hovertemplate="<b>%{label}</b><extra></extra>",
     marker=dict(line=dict(color="black", width=1))
 )
